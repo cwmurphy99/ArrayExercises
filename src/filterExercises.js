8dff6c9388f1ca8bc6c2de538e17a3d592a2cc52
Chris Murphy <theworldisyours@gmail.com>
2021-11-12 09:56:04 -0500

    first push

--- src/filterExercises.js
+++ src/filterExercises.js
@@ -3,26 +3,42 @@ import { useStudents, useInstructors } from "./data/classroom.js";
 const students = useStudents();
 const instructors = useInstructors();
 
+
 // Export a function named getStudentsInCohort
 // It should accept one integer parameter named `cohort`
 // It should return an array of just the students who are in that cohort
+export const getStudentsInCohort = (cohort) => {
+  return students.filter(students => students.cohort === cohort)
+};
 
 // Export a function called getFullTimeStudents
 // It should not accept any parameters
 // It should return an array of only the full time students
+export const getFullTimeStudents = () => {
+    return students.filter(students => students.fullTime === true)
+}
 
 // Export a function called getStudentsByInstructorId
 // It should accept one integer parameter name `instructorId`
 // It should return an array of students with that instructor
+export const getStudentsByInstructorId = (instructorId) => {
+    return students.filter(students => students.instructorId === instructorId)
+}
 
 // Export a function called getPolyglotStudents
 // It should accept one integer parameter named `languageCount`
 // It should return an array of students who know as many (or more) languages than `languageCount`
 // Ex: If the number 2 is passed to the function, only the students who know 2 or more languages should be returned
+export const getPolyglotStudents = (languageCount) => {
+    return students.filter(students => students.languages.length >= languageCount)
+}
 
 // Export a function called getAvailableInstructors
 // It should not accept any parameters
 // It should return an array of instructors that don't have any students
+// export const getAvailableInstructors = () => {
+
+// }
 
 // Export a function called getStudentsByLanguage
 // It should accept one string parameter named `language`
